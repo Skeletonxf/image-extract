ecf3ee2c6703a2149b5d958a6c38b40c58fe1f19
Skeletonxf <skeletonxf@gmail.com>
2020-06-14 12:34:44 +0100

    Add notice on android about shortcuts not being supported

--- settings/settings.js
+++ settings/settings.js
@@ -14,3 +14,20 @@ for (let property in
     core.settings.syncLocalStorage(property)
   })
 }
+
+// will be undefined on android
+if (browser.commands === undefined) {
+    document.addEventListener('DOMContentLoaded', () => {
+        // Disable settings that don't work on Android
+        function disable(setting) {
+            setting.style.opacity = 0.5
+            setting.style.pointerEvents = 'none'
+            let text = document.createElement('strong')
+            text.textContent = 'Unsupported on your device/browser version'
+            text.style.fontSize = 'large'
+            text.style.color = 'darkred'
+            setting.prepend(text)
+        }
+        disable(document.querySelector('#commandSettings'))
+    })
+}
