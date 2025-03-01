54459eb213c968dac88887653b47cfef9fdc7e6e
Skeletonxf <skeletonxf@gmail.com>
2025-03-01 15:04:31 +0000

    Initial work on definining new tab page HTML
    See https://github.com/Skeletonxf/image-extract/issues/1

--- src/image-extract.js
+++ src/image-extract.js
@@ -2,39 +2,51 @@ import settings from '/src/settings.js'
 import console from '/src/logger.js'
 
 export default class ImageExtract {
-    // TODO: To run after images are extracted, in a new tab
-    // async extractImages(tab /* Tab */) {
-    //     try {
-    //         let js = await browser.scripting.executeScript({
-    //             target: {
-    //                 tabId: tab.id,
-    //             },
-    //             files: [ "/src/content-script.js" ],
-    //         })
-    //         let css = browser.scripting.insertCSS({
-    //             target: {
-    //                 tabId: tab.id,
-    //             },
-    //             files: [ "/src/content.css" ],
-    //         })
-    //         await js
-    //         await css
-    //     } catch (error) {
-    //         console.error('Error attempting to extract images', error)
-    //     }
-    // }
-
     async extractImages(tab /* Tab */) {
         try {
             let js = await browser.scripting.executeScript({
                 target: {
                     tabId: tab.id,
                 },
-                files: [ "/src/extraction-script.js" ],
+                files: [ "/src/content-script.js" ],
+            })
+            let css = browser.scripting.insertCSS({
+                target: {
+                    tabId: tab.id,
+                },
+                files: [ "/src/content.css" ],
             })
             await js
+            await css
         } catch (error) {
             console.error('Error attempting to extract images', error)
         }
     }
+
+    // async showExtractedImages() {
+    //     try {
+    //         let tab = await browser.tabs.create({
+    //             url: '/src/page/page.html',
+    //             active: true,
+    //             // TODO: Consider putting this new tab just after the old tab if
+    //             // possible
+    //         })
+    //     } catch (error) {
+    //         console.error('Error attempting to create extracted images tab', error)
+    //     }
+    // }
+    //
+    // async extractImages(tab /* Tab */) {
+    //     try {
+    //         let js = await browser.scripting.executeScript({
+    //             target: {
+    //                 tabId: tab.id,
+    //             },
+    //             files: [ "/src/extraction-script.js" ],
+    //         })
+    //         await js
+    //     } catch (error) {
+    //         console.error('Error attempting to extract images', error)
+    //     }
+    // }
 }
---
+++ src/page/main.js
@@ -0,0 +1,2 @@
+// TODO: Request from add on the list of extracted images
+// then display them on the page
