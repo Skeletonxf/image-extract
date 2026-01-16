8c780951dcb0b9c94b2ef7927650a1444908166b
Karaviro <131587924+Karaviro@users.noreply.github.com>
2026-01-16 04:55:49 +0100

    Delete settings directory

--- settings/defaults.js
+++
@@ -1,9 +0,0 @@
-export default {
-    centerImages : true,
-    realSizeImages : false,
-    showBackgroundImages : false,
-    confirmBeforeRunningImageExtract: false,
-    keyboardShortcut1Enabled: false,
-    keyboardShortcut2Enabled: false,
-    keyboardShortcut3Enabled: false
-}
