11470304afd7ee6e06dd3a89f1c0d120153c7495
Skeletonxf <skeletonxf@gmail.com>
2026-07-18 17:09:24 +0100

    Add note to push script
    https://github.com/mozilla/web-ext/issues/2206

--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
 # See https://extensionworkshop.com/documentation/develop/developing-extensions-for-firefox-for-android/
+# Using a custom app icon can cause errors like 'Activity class {org.mozilla.firefox_beta/org.mozilla.firefox_beta.App} does not exist' so ensure to remove any custom icon if such an error happens
 web-ext run -t firefox-android --adb-device <your ADB device, can find from `adb devices`> --firefox-apk <firefox package name, e.g. org.mozilla.fennec_fdroid or org.mozilla.firefox_beta>
